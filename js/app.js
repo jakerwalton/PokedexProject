11afb21e81d8b1480fbde16086df0e8f189e08f0
Jake Walton <jakewalton@Mac-mini.local>
2022-05-08 22:27:17 -0500

    wip second commit: script works and displays information to the DOM

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-// window.onload = function() {
-//     if (window.jQuery) {  
-//         // jQuery is loaded  
-//         alert("Yeah!");
-//     } else {
-//         // jQuery is not loaded
-//         alert("Doesn't Work");
-//     }
-// }
-
-let pokeData, userInput;
-
-const $name = $('#name');
+const $name = $("#name");
+const $numId = $("#idNum");
+const $elType = $("#elemType");
+const $description = $("#description");
+const $height = $("#height");
+const $weight = $("#weight");
 const $input = $('input[type="text"]');
+const $image = $("#sprite");
 
-$(document).on('submit', getData);
+$("form").on("submit", getData);
 
 function getData(e) {
-    e.preventDefault();
-    userInput = $input.val();
-    $.ajax({url:"https://pokeapi.co/api/v2/pokemon/ditto"
-        }).then(function(data) {
-            console.log(data)
-            pokeData = data;
-            $input.val("")
-            render();
-        },function(error) {
-            console.log('bad request', error);
-        }
-    );
+  e.preventDefault();
+  userInput = $input.val();
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${userInput.toLowerCase()}`,
+  })
+  .then(
+    function (data) {
+      console.log(data);
+      apiData = data;
+      $input.val("");
+      display();
+    },
+    function (error) {
+      console.log("bad request", error);
+    }
+  );
 }
-
-function render() {
-    $name.text(pokeData.name);
+function display() {
+  let height = apiData.height * 0.1;
+  let weight = apiData.weight * 0.1;
+  $name.text(apiData.name);
+  $numId.text("ID " + apiData.id);
+  $elType.text(apiData.types[0].type.name);
+  $height.text(height.toFixed(1) + " m");
+  $weight.text(weight.toFixed(1) + " kg");
+  $image.attr("src", apiData.sprites.front_default);
 }
