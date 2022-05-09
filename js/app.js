0a3670137138a90b6b51c5a393e12b8148bbbbb8
Jake Walton <jakewalton@Mac-mini.local>
2022-05-09 10:46:13 -0500

    wip 3rd commit: Now have second function to grab brief description from separate api link

--- js/app.js
+++ js/app.js
@@ -14,20 +14,28 @@ function getData(e) {
   userInput = $input.val();
   $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${userInput.toLowerCase()}`,
-  })
-  .then(
-    function (data) {
+  }).then(function (data) {
       console.log(data);
       apiData = data;
       $input.val("");
-      display();
+      descrInfo();
     },
     function (error) {
       console.log("bad request", error);
     }
   );
 }
-function display() {
+function descrInfo() {
+  let idValue = apiData.id;
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/characteristic/${idValue}`,
+  }).then(function (data) {
+    console.log(data);
+    descrData = data;
+    displayInfo();
+  });
+}
+function displayInfo() {
   let height = apiData.height * 0.1;
   let weight = apiData.weight * 0.1;
   $name.text(apiData.name);
@@ -36,4 +44,5 @@ function display() {
   $height.text(height.toFixed(1) + " m");
   $weight.text(weight.toFixed(1) + " kg");
   $image.attr("src", apiData.sprites.front_default);
+  // $description.text(descrData.descriptions[7].description);
 }
